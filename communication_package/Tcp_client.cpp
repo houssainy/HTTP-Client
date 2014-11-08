333c8cf29bc79f319333c0c3287ca40e8b9ac1e1
AhmedAbdelmajied <ahmed.abdelmajied@gmail.com>
2014-11-08 18:30:22 +0200

    add methods and parameters to Tcp_client class

--- communication_package/Tcp_client.cpp
+++ communication_package/Tcp_client.cpp
@@ -1,10 +1,33 @@
 #include "Tcp_client.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 
-Tcp_client::Tcp_client()
-{
-    //ctor
+Tcp_client::Tcp_client(int port_number,char* hostname){
+
 }
 
+void Tcp_client::connect(){
+}
+
+
+void Tcp_client::send(void *data){
+
+}
+
+void* Tcp_client::receive(){
+
+return 0;
+}
+
+void Tcp_client::close(){
+
+}
 Tcp_client::~Tcp_client()
 {
     //dtor
--- communication_package/Tcp_client.h
+++ communication_package/Tcp_client.h
@@ -1,14 +1,35 @@
 #ifndef TCP_CLIENT_H
 #define TCP_CLIENT_H
+#include <netinet/in.h>
+#include <netdb.h>
 
+/**
+* author Ahmed Abdelmajied
+*
+* Tcp_client class is implementation of TCP protocol in clients that's used to
+* send and receive data from server and client and verse vise.
+*/
 
 class Tcp_client
 {
-    public:
-        Tcp_client();
-        virtual ~Tcp_client();
-    protected:
-    private:
+public:
+    // Constructor
+    Tcp_client(int port_number,char* hostname);
+    // Connect Method used to make client connect with server
+    void connect();
+    // Send method used to send data to server
+    void send(void *data);
+    // Receive method used to receive data from server
+    void* receive();
+    // close method used to close open connection with server
+    void close();
+    virtual ~Tcp_client();
+protected:
+
+private:
+ int sockfb , portnum , n ;
+ struct sockaddr_in serv_addr;
+ struct hostent *server;
 };
 
 #endif // TCP_CLIENT_H
