1b55cba41734a7f82545525abbab499e9f8d4ff2
Houssainy <mohamed.houssainy@gmail.com>
2014-11-08 17:04:51 +0200

    First commit

---
+++ communication_package/Tcp_client.cpp
@@ -0,0 +1,11 @@
+#include "Tcp_client.h"
+
+Tcp_client::Tcp_client()
+{
+    //ctor
+}
+
+Tcp_client::~Tcp_client()
+{
+    //dtor
+}
---
+++ communication_package/Tcp_client.h
@@ -0,0 +1,14 @@
+#ifndef TCP_CLIENT_H
+#define TCP_CLIENT_H
+
+
+class Tcp_client
+{
+    public:
+        Tcp_client();
+        virtual ~Tcp_client();
+    protected:
+    private:
+};
+
+#endif // TCP_CLIENT_H
