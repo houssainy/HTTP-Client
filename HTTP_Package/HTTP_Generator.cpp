e8b9ac3da22f8009eeac09e0895014539fe1cee0
AhmedAbdelmajied <ahmed.abdelmajied@gmail.com>
2014-11-10 10:16:57 +0200

    finish get and post request message

--- HTTP_Package/HTTP_Generator.cpp
+++ HTTP_Package/HTTP_Generator.cpp
@@ -3,14 +3,87 @@
 
 HTTP_Generator::HTTP_Generator()
 {
-    //ctor
-}
-
-void HTTP_Generator::generate_get_request(string file_name ,unsigned char HTTP_type ){
 
 }
 
+string HTTP_Generator::generate_get_request(string file_name ,string HTTP_type )
+{
 
+    string msg ("GET "+file_name+" "+HTTP_type+"\r\n");
+    msg +="Host: "+get_host_name()+"\r\n";
+    msg +="Accept: "+get_accepted_types()+"\r\n";
+    msg +="Accept-Language: "+get_accepted_language()+"\r\n";
+    msg +="User-Agent: "+get_user_agent()+"\r\n";
+
+    return msg ;
+}
+
+string HTTP_Generator::generate_post_request(string file_name ,string HTTP_type , string file_type , char data[] )
+{
+    string msg ("GET "+file_name+" "+HTTP_type+"\r\n");
+    msg +="Host: "+get_host_name()+"\r\n";
+    msg +="Accept: "+get_accepted_types()+"\r\n";
+    msg +="Accept-Language: "+get_accepted_language()+"\r\n";
+    msg +="User-Agent: "+get_user_agent()+"\r\n";
+    msg +="Content-Type: "+file_type+"\r\n";
+    msg +="Content-Lenght: "+to_string(sizeof(data))+"\r\n\n";
+    msg +=data;
+
+    return msg ;
+}
+
+
+string HTTP_Generator::generate_get_response()
+{
+
+    string msg ("");
+
+    return msg ;
+
+}
+
+
+string HTTP_Generator::generate_post_response()
+{
+
+    string msg ("");
+
+    return msg ;
+
+}
+
+string HTTP_Generator::get_host_name ()
+{
+
+    ///TODO get hostname
+
+    return ("MeGoFilio");
+}
+
+string HTTP_Generator::get_accepted_language ()
+{
+
+    ///TODO get accepted language from system
+
+    return ("en-us , ar");
+}
+
+string HTTP_Generator::get_user_agent()
+{
+
+    /// TODO get user agent from system
+
+    return ("Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)");
+}
+
+string HTTP_Generator::get_accepted_types()
+{
+
+    ///TODO get accepted files from system
+
+    return ("*/*");
+
+}
 
 HTTP_Generator::~HTTP_Generator()
 {
--- HTTP_Package/HTTP_Generator.h
+++ HTTP_Package/HTTP_Generator.h
@@ -2,19 +2,34 @@
 #define HTTP_GENERATOR_H
 #include <string>
 
-
 using namespace std ;
 class HTTP_Generator
 {
     public:
-        static const unsigned char HTTP1 = 1;
-        static const unsigned char HTTP2 = 2;
+
+        const static string HTTP1 ;
+        const static string HTTP2 ;
+        const static string image ;
+        const static string txt   ;
 
         HTTP_Generator();
-        void generate_get_request(string file_name ,unsigned char HTTP_type );
+        string generate_get_request (string file_name ,string HTTP_type );
+        string generate_post_request(string file_name ,string HTTP_type , string file_type , char data[] );
+
+        string generate_get_response();
+        string generate_post_response();
         virtual ~HTTP_Generator();
-    protected:
+
+
     private:
+        string get_host_name ();
+        string get_accepted_language ();
+        string get_user_agent();
+        string get_accepted_types();
 };
 
+    const string HTTP_Generator::HTTP1 = "HTTP/1.0" ;
+    const string HTTP_Generator::HTTP2 = "HTTP/1.1" ;
+    const string HTTP_Generator::image = "image/*"  ;
+    const string HTTP_Generator::txt   = "text/html";
 #endif // HTTP_GENERATOR_H
