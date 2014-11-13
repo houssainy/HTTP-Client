b99d0ec575cfa1e8967a6bc15a1bf2a1ec18e8b0
Houssainy <mohamed.houssiany@gmail.com>
2014-11-13 20:47:23 +0200

    testing

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <string>
 #include "HTTP_Package/HTTP_Generator.h"
 
+#include <iostream>
+
 const string HTTP_Generator::HTTP1 = "HTTP/1.0" ;
 const string HTTP_Generator::HTTP2 = "HTTP/1.1" ;
 const string HTTP_Generator::image = "image/*"  ;
@@ -14,7 +16,7 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 
-    HTTP_Generator gen ;
+    /*HTTP_Generator gen ;
     ifstream inFile;
 	size_t size = 0;
 	inFile.open("test.txt", ios::in|ios::binary|ios::ate);
@@ -34,6 +36,35 @@ int main(int argc, char *argv[])
 
 
     string msg = gen.generate_post_request("Ahmed",HTTP_Generator::HTTP2,HTTP_Generator::txt , data);
-    cout << msg <<endl;
+    cout << msg <<endl;*/
 
+    /*if(argc < 3) {
+        cout << "Missing arguments!" << endl;
+        exit(EXIT_FAILURE);
+    }*/
+    int portNum = 8080/*atoi(argv[2])*/;
+
+    Tcp_client tcp_client(portNum, /*argv[1]*/ "localhost");
+    tcp_client.connect_to_server();
+
+    //cout << "Connected to "<< argv[1] << " on port " << argv[2] << endl;
+    char msg[] = "Hello From client!";
+    tcp_client.send(msg, sizeof(msg));
+
+    char * z = tcp_client.receive();
+    cout<< "Received: " << z << endl;
+
+    // Receive file
+    // Receive size
+    ofstream myfile;
+    myfile.open("example.txt");
+    string line = "";
+    while((line = tcp_client.receive()) != "\0") {
+        myfile << line << endl;
+    }
+
+    myfile.close();
+
+    tcp_client.close_connection();
+    return 0;
 }
